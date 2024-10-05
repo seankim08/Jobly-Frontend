55b8b4bc534a5ab0b640a1ee6c2ef506e705aeb7
Sean <seanghunkim1006@gmail.com>
2024-10-05 13:35:13 -0400

    small change to api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "https://jobly-backend-pyv3.onrender.com";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001"
 
 class JoblyApi {
   static token;
